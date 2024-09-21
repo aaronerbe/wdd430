3298f7cbebecd216304945463a2207388ac5ffa2
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2024-09-20 18:41:26 -0600

    adding dist back to .gitignore, moving contents of dist to a dir called todo under wdd430.  set base in the vite.config.js to /wdd430/todo

--- wk1/todo/vite.config.js
+++ wk1/todo/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wdd430/wk1/todo/'
+  base: '/wdd430/todo/'
 })
